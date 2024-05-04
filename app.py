7b884149f9b01f9c97e1e77d5fe5ddc47568d1d4
Sumin-kimmmm <kimsu081200@naver.com>
2024-05-04 23:17:30 +0900

    week2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
 
@@ -9,6 +9,10 @@ def basic():
 @app.route('/info')
 def info():
     return render_template("info.html")
+
+@app.route('/activity')
+def activity_page():  # 함수 이름 변경
+    return render_template("activity.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
